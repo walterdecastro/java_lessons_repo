54a6cc04bc016854a58f9439feddebd7032a316c
walterdecastro <walterdc1995@gmail.com>
2022-08-07 16:49:33 -0300

    bank account lesson

---
+++ java_lessons/src/java_lessons/Conta.java
@@ -0,0 +1,12 @@
+package java_lessons;
+
+public class Conta {
+	
+	String cliente;
+	double saldo;
+	
+	void exibeSaldo() {
+		System.out.println(cliente + " seu saldo e " + saldo);
+	}
+
+}
---
+++ java_lessons/src/java_lessons/ContaTeste.java
@@ -0,0 +1,14 @@
+package java_lessons;
+
+public class ContaTeste {
+
+	public static void main(String[] args) {
+		
+		Conta conta = new Conta();
+		conta.cliente = "John Lennon";
+		conta.saldo = 10000;
+		conta.exibeSaldo();
+
+	}
+
+}
